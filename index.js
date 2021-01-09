dc67ace1183386f9720808a4160f92170c05b8e9
akinjed5 <62756856+akinjed5@users.noreply.github.com>
2021-01-09 13:53:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var newInput1= "images/dice" + randomNumber1 + ".png"
+var newInput2= "images/dice" + randomNumber2 + ".png"
+document.querySelector(".img1").setAttribute("src", newInput1)
+document.querySelector(".img2").setAttribute("src", newInput2)
+if (newInput1>newInput2){document.querySelector("h1").textContent="Player 1 wins"}
+else if (newInput1<newInput2){document.querySelector("h1").textContent="Player 2 wins"}
+else if (newInput1=newInput2){document.querySelector("h1").textContent="Draw"}
