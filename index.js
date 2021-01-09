744edc2b87032d64df73108887719dc89b659b0b
akinjed5 <62756856+akinjed5@users.noreply.github.com>
2021-01-09 13:55:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var newInput1= "images/dice" + randomNumber1 + ".png"
-var newInput2= "images/dice" + randomNumber2 + ".png"
+var newInput1= "dice" + randomNumber1 + ".png"
+var newInput2= "dice" + randomNumber2 + ".png"
 document.querySelector(".img1").setAttribute("src", newInput1)
 document.querySelector(".img2").setAttribute("src", newInput2)
 if (newInput1>newInput2){document.querySelector("h1").textContent="Player 1 wins"}
